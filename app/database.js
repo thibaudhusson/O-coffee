c02b89780134b0c6c32f0068f275a08a80083878
thibaud husson <thibaud.husson@oclock.shcool>
2025-07-21 19:39:39 +0200

    update deploiement

--- app/database.js
+++ app/database.js
@@ -1,7 +1,12 @@
 import pg from 'pg';
 
-const client = new pg.Client(process.env.PG_URL);
+const client = new pg.Client({
+  connectionString: process.env.PG_URL,
+  ssl: {
+    rejectUnauthorized: false // Autorise les certificats auto-signés (OK pour Render)
+  }
+});
 
-await client.connect()
-;
+await client.connect();
+
 export default client;
